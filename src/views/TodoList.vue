003f86a935bf6ac1f3945e46d3ea183f74647ba9
somayeh ehteshami <soehteshami@gmail.com>
2022-03-27 17:12:30 +0430

    add new todo in list

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,7 +1,60 @@
 <template>
   <div>
-    <Button variant="contained" color="primary"> Primary </Button>
-    <TextField label="Outlined" variant="outlined" />
+    <input v-model="newTodo" />
+    <button @click="addNewTodo">add</button>
+    <hr style="margin-top: 20px; margin-bottom: 20px" />
+
+    <div v-if="todos.length > 0">
+      <ul v-for="todo in todos" :key="todo.id">
+        <li @click="statusChange(todo)" :class="`${todo.status ? 'liClass done' : 'liClass'}`">
+          {{ todo.title }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>there is not any todo</div>
   </div>
 </template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addNewTodo() {
+      let Todo = {
+        id: new Date().getTime(),
+        title: this.newTodo,
+        status: false,
+      };
+
+      this.todos.push(Todo);
+
+      this.newTodo = "";
+    },
+    statusChange (todo) {
+       this.todos = this.todos.map(item => {
+         if (item.id === todo.id) {
+           item.status = !item.status
+         }
+         return item
+       })
+    }
+  },
+};
+</script>
+
+<style>
+.liClass {
+  color: blue;
+  cursor: pointer;
+}
+.done {
+  color: green;
+}
+</style>
 
